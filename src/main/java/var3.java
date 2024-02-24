08a8abc24f8befaff1e4a03210e6fa29939d5996
Andrey <gorohov1327@gamil.com>
2024-02-24 21:58:28 +0400

    Complete?

---
+++ src/main/java/var3.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class var3 {
+      public static void main(String[] args) {
+          char x=0;
+
+        System.out.println(charToNum(x));
+
+    }
+
+        public static int charToNum(char x) {
+          x= '8';
+          int y= x;
+            return y;
+
+
+        }
+}
+
+
+
+
+        //System.out.println("Введи число от 0 до 9: ");
+            
+            /*int chislo = new Scanner(System.in).nextInt();
+            if (chislo >= 0 && chislo <= 9)
+                System.out.println(charToNum();
+            else System.out.println("Не верный символ. Game over.");
+           */
+
+        //}
+            //if (Character.isUpperCase(x)) {
+             //   return (int) x +0;
+        //    } else {
+         //       return (int) x+0;;
+     //   }
+/*
+Дана сигнатура public static int charToNum(char x){
+метод принимает символ Х который представляет собой один из 123456789
+необходимо реализовать метод чтобы он преобразовывал символ в соответсвующее число
+при реализации метода не использовать методы класса character
+подсказка. код символа 0 это число 48
+
+пример Х='3'
+результаты 3
+
+ */
